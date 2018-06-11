008d8ad23d8e512fcb339f07f0c994f8fc3418e2
impulse-x <40152105+impulse-x@users.noreply.github.com>
2018-06-12 00:04:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -180,12 +180,10 @@ if (reaction.emoji.name === '❎'){
         if(args[0] === 'join') return message.member.voiceChannel.join(); message.channel.send("осуществлен вход в канал: **"+ message.member.voiceChannel.name + "**")
         if(args[0] === 'leave') return message.member.voiceChannel.leave(); message.channel.send("осуществлен выход из канала: **"+ message.member.voiceChannel.name + "**")
     } else if(['ascii'].includes(command)) {
-        message.channel.send('<a:loading:435849475865575424> Обрабатываю запрос...').then(msg => {
+        message.channel.send('<a:loading:435849475865575424> Обрабатываю запрос...')
         request('http://artii.herokuapp.com/make?text='+args.join(' '), function (error, response, body) {
-message.channel.send("```"+body+"```");
+message.channel.edit("```"+body+"```")
             }
-});
-
     } else if(['google'].includes(command)) {
 // Depending on your command framework (or if you use one), it doesn't have to
 // edit messages so you can rework it to fit your needs. Again, this doesn't have
