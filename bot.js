a669f3636bdb95bb70f0ebc0037865930a45dcd8
impulse-x <40152105+impulse-x@users.noreply.github.com>
2018-06-13 12:58:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1253,6 +1253,27 @@ message.channel.send('RAS');
                 } catch (e) {console.log(e)}
             });
         });
+    } 
+    else if(['feed'].includes(command)) {
+        message.delete();
+        let user = message.author;
+        let user1 = message.mentions.users.first();
+        if (!user1 || user1.id === user.id) {
+            user = client.user;
+            user1 = message.author;
+        }
+        message.channel.send('Загрузка...').then(msg => {
+            request('https://nekos.life/api/v2/img/feed', function (error, response, body) {
+                try {
+                    let arr = JSON.parse(body);
+                    let embed = new Discord.RichEmbed()
+                        .setDescription(`${user} дал(а) покушать ${user1}`)
+                        .setImage(arr['url'])
+                        .setColor('#ffff00');
+                    msg.edit(`${user1}`, {embed});
+                } catch (e) {console.log(e)}
+            });
+        });
     } else if(['cat'].includes(command)) {
         message.channel.send('Загрузка...').then(msg => {
             request('https://nekos.life/api/v2/img/meow', function (error, response, body) {
