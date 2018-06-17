f31aa2d4aafc2ebd1824c3101fc6070b2e758f28
impulse-x <40152105+impulse-x@users.noreply.github.com>
2018-06-18 00:15:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,6 +18,13 @@ const prefix = "x!";
 const creators = ['361951318929309707'];
 //массив эмодзи
 const emojis = {nya:'435849475865575424'}
+let actFUN = 0;  // actFUN = actFUN + 1;actALL = actALL +1;
+let actMOD = 0;  // actMOD = actMOD + 1;actALL = actALL +1;
+let actRCT = 0;  // actRCT = actRCT + 1;actALL = actALL +1;
+let actNSFW = 0; // actNSFW = actNSFW + 1;actALL = actALL +1;
+let actOWN = 0; //  actOWN = actOWN + 1;actALL = actALL +1;
+let actIMG = 0; //  actIMG = actIMG + 1;actALL = actALL +1;
+let actALL = 0; //  actALL = actALL +1;actALL = actALL +1;
 //массив цветов
 const colors = ['ff2828','ff3d28','ff4b28','ff5a28','ff6828','ff7628','ff8c28','ffa128','ffac28','ffb728','ffc228','ffd028','ffd728','ffe228','fff028','fffb28','edff28','deff28','d0ff28','c2ff28','b3ff28','9aff28','8cff28','7dff28','6fff28','5aff28','3dff28','28ff2b','28ff41','28ff56','28ff6c','28ff81','28ff93','28ffa9','28ffba','28ffc9','28ffde','28fff4','28ffff','28f0ff','28deff','28deff','28d3ff','28c5ff','28baff','28b0ff','28a5ff','289eff','2893ff','2885ff','2876ff','2864ff','2856ff','284bff','2841ff','2836ff','2828ff','3228ff','4428ff','5328ff','6828ff','7628ff','7e28ff','8828ff','9328ff','a128ff','b028ff','be28ff','c928ff','d328ff','db28ff','e528ff','f028ff','ff28ff','ff28f7','ff28e5','ff28de','ff28d0','ff28c9','ff28ba','ff28b3','ff28a5','ff289a','ff288c','ff2881','ff287a','ff2873','ff2868','ff2861','ff2856','ff284f','ff2848','ff2844','ff282b'];
 //Выполняет действие когда бот запустился.
@@ -212,6 +219,7 @@ client.on('message', async (message) => {
 	    message.channel.send(ttsmessage, {tts: true});
 	    message.delete(); 
     } else if(['iinvite', 'inviteInfo', 'infoInvite'].includes(command)) {
+	    actFUN = actFUN + 1;actALL = actALL +1;
 	    let invi = args.join(" ")
   let invite = await client.fetchInvite(invi)
   if(!invite) return message.reply("Пожалуйста укажите приглашение")
@@ -227,6 +235,7 @@ client.on('message', async (message) => {
   .setThumbnail(`https://cdn.discordapp.com/icons/${invite.guild.id}/${invite.guild.icon}.png`)
 
   message.channel.send(embed);
+  
     } else if(['save'].includes(command)) {
 	    message.channel.send("**Disclaimer:** ваш ключ сохранен не навсегда, ключ будет удален при перезапуске бота.");
 			if(args.length < 2){
@@ -790,6 +799,7 @@ client.guilds.forEach((guild) => {users += client.users.size});
         embed.addField('Количество серверов', client.guilds.size)
         embed.addField('Количество пользователей', client.users.size)
         embed.addField('Количество каналов', client.channels.size)
+        embed.addField('actFUN' actFUN)
         embed.addField('Со-Авторы', '<@421030089732653057>')
         message.channel.send(embed);
         message.delete();
