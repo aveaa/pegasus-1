227cc70ad90207c9c19bf545fe9a41e87267a5f4
impulse-x <40152105+impulse-x@users.noreply.github.com>
2018-06-17 09:57:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -381,36 +381,21 @@ message.edit("```"+body+"```");
     }).catch(function() {});
 });
     } else if(['google'].includes(command)) {
-// Depending on your command framework (or if you use one), it doesn't have to
-// edit messages so you can rework it to fit your needs. Again, this doesn't have
-// to be async if you don't care about message editing.
-async function googleCommand(msg, args) {
+//let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(args.join(' '))}`;
+  message.channel.send(`Ищу в google ${args.join(' ')}...`).then(m => m.delete(2500))
+  return snekfetch.get(searchUrl).then((result) => {
 
-   // These are our two variables. One of them creates a message while we preform a search,
-   // the other generates a URL for our crawler.
-   let searchMessage = await message.reply('Searching... Sec.');
-   let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(msg.content)}`;
+    // Cheerio lets us parse the HTML on our google result to grab the URL.
+    let $ = cheerio.load(result.text);
+    let googleData = $('.r').first().find('a').first().attr('href');
 
-   // We will now use snekfetch to crawl Google.com. Snekfetch uses promises so we will
-   // utilize that for our try/catch block.
-   return snekfetch.get(searchUrl).then((result) => {
+    googleData = querystring.parse(googleData.replace('/url?', ''));
+    message.channel.send(`Найден результат по запросу ${args.join(' ')}:\n${googleData.q}`)
 
-      // Cheerio lets us parse the HTML on our google result to grab the URL.
-      let $ = cheerio.load(result.text);
-
-      // This is allowing us to grab the URL from within the instance of the page (HTML)
-      let googleData = $('.r').first().find('a').first().attr('href');
-
-      // Now that we have our data from Google, we can send it to the channel.
-      googleData = querystring.parse(googleData.replace('/url?', ''));
-      searchMessage.edit(`Result found!\n${googleData.q}`);
-
-  // If no results are found, we catch it and return 'No results are found!'
   }).catch((err) => {
-     searchMessage.edit('No results found!');
+    message.channel.send(`По запросу ${args.join(' ')} ничего не найдено...`)
   });
-}
-    } else if(['pin'].includes(command) && message.member.hasPermission('MANAGE_MESSAGES')) {
+} else if(['pin'].includes(command) && message.member.hasPermission('MANAGE_MESSAGES')) {
         let kanal = (args[0])
         let sms = (args[1])
         
