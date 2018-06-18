84edf45bd9796a8db1a731cf77d87ccc8117e219
impulse-x <40152105+impulse-x@users.noreply.github.com>
2018-06-18 12:52:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1105,7 +1105,7 @@ message.guild.channels.filter(chan => chan.type === 'voice').forEach((channel) =
         const embed = new Discord.RichEmbed()
             .setTitle('Приглашение бота на ваш сервер.')
             .setColor("#0000ff")
-            .setDescription("Ссылка на бота https://discordapp.com/oauth2/authorize?&client_id=441667160025333762&scope=bot&permissions=8 \nЕсли вы не желаете давать боту права `SERVER_MANAGE` то перейдите по данной ссылке: \nhttps://discordapp.com/oauth2/authorize?&client_id=441667160025333762&scope=bot&permissions=0")
+            .setDescription(`Ссылка на бота https://discordapp.com/oauth2/authorize?&client_id=${client.user.id}&scope=bot&permissions=8 \nЕсли вы не желаете давать боту права `SERVER_MANAGE` то перейдите по данной ссылке: \nhttps://discordapp.com/oauth2/authorize?&client_id=${client.user.id}&scope=bot&permissions=0`)
             .setFooter(message.channel.guild.name)
             .setTimestamp(); message.react("✅"); console.log(`${message.author} использовал invite`)
         message.channel.send({embed});
